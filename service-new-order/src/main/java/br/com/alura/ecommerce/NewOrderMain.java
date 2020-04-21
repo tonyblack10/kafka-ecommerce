f266785f66b0b13fc3a11944bca8153433de3934
Tony Costa <tonyblack10@gmail.com>
2020-04-21 09:47:41 -0300

    microsserviços como módulos em um mono repo

--- service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
-public class NewOrder {
+public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
         try(var orderDispatcher = new KafkaDispatcher<Order>()) {
