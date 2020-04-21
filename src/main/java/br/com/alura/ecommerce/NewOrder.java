bd0d326c55d5ca78ce8df19a9d82952423eede81
Tony Costa <tonyblack10@gmail.com>
2020-04-21 05:21:35 -0300

    vários consumidores e produtores

--- src/main/java/br/com/alura/ecommerce/NewOrder.java
+++ src/main/java/br/com/alura/ecommerce/NewOrder.java
@@ -1,5 +1,6 @@
 package br.com.alura.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -16,13 +17,18 @@ public class NewOrder {
         var value = "13455,89985,12345";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
 
-        producer.send(record, (data, ex) -> {
+        Callback callback = (data, ex) -> {
             if (Objects.nonNull(ex)) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + " / offset " + data.offset() + " / timestamp " + data.timestamp());
-        }).get();
+        };
+        producer.send(record, callback).get();
+
+        var email = "Thank you for your order! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
