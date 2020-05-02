ff5cc4344f5c2470f5f12a6255b5f2448a2b9692
Tony Costa <tonyblack10@gmail.com>
2020-05-02 18:25:22 -0300

    simulando a geração de relatórios

---
+++ service-reading-report/src/main/java/br/com/alura/ecommerce/ReadingReportService.java
@@ -0,0 +1,38 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class ReadingReportService {
+
+    private static Path SOURCE = new File("src/main/resources").toPath();
+
+    public static void main(String[] args) throws IOException {
+        var reportService = new ReadingReportService();
+        try(var service = new KafkaService<>(ReadingReportService.class.getSimpleName(),
+                "USER_GENERATE_READING_REPORT",
+                reportService::parse,
+                User.class,
+                Map.of())) {
+            service.run();
+        }
+
+    }
+
+    private void parse(ConsumerRecord<String, User> record) throws IOException {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing report for " + record.value());
+
+        var user = record.value();
+        var target = new File(user.getReportPath());
+
+        IO.copyTo(SOURCE, target);
+        IO.append(target, "Created for " + user.getUuid());
+
+        System.out.println("File created: " + target.getAbsolutePath());
+    }
+}
