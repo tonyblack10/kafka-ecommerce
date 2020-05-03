f20e073eac0634b0527935bdbd8a6b9758b48280
Tony Costa <tonyblack10@gmail.com>
2020-05-03 17:15:53 -0300

    offset latest e earliest

--- common-kafka/src/main/java/br/com/alura/ecommerce/consumer/ConsumerService.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/consumer/ConsumerService.java
@@ -3,11 +3,9 @@ package br.com.alura.ecommerce.consumer;
 import br.com.alura.ecommerce.Message;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.io.IOException;
-
 public interface ConsumerService<T> {
 
-    void parse(ConsumerRecord<String, Message<T>> record) throws IOException;
+    void parse(ConsumerRecord<String, Message<T>> record) throws Exception;
     String getTopic();
     String getConsumerGroup();
 
--- service-email-new-order/src/main/java/br/com/alura/ecommerce/EmailNewOrderService.java
+++ service-email-new-order/src/main/java/br/com/alura/ecommerce/EmailNewOrderService.java
@@ -1,29 +1,21 @@
 package br.com.alura.ecommerce;
 
-import br.com.alura.ecommerce.consumer.KafkaService;
+import br.com.alura.ecommerce.consumer.ConsumerService;
+import br.com.alura.ecommerce.consumer.ServiceRunner;
 import br.com.alura.ecommerce.dispatcher.KafkaDispatcher;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.io.IOException;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-public class EmailNewOrderService {
+public class EmailNewOrderService implements ConsumerService<Order> {
 
     private final KafkaDispatcher<Email> emailDispatcher = new KafkaDispatcher<>();
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
-        var emailService = new EmailNewOrderService();
-        try(var service = new KafkaService<>(EmailNewOrderService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER",
-                emailService::parse,
-                Map.of())) {
-            service.run();
-        }
-
+    public static void main(String[] args) {
+        new ServiceRunner(EmailNewOrderService::new).start(1);
     }
 
-    private void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
+    public void parse(ConsumerRecord<String, Message<Order>> record) throws ExecutionException, InterruptedException {
         System.out.println("------------------------------------------");
         System.out.println("Processing new order, preparing email");
         var message = record.value();
@@ -37,5 +29,15 @@ public class EmailNewOrderService {
                 order.getEmail(),
                 id,
                 emailCode);
+    }
+
+    @Override
+    public String getTopic() {
+        return "ECOMMERCE_NEW_ORDER";
+    }
+
+    @Override
+    public String getConsumerGroup() {
+        return EmailNewOrderService.class.getSimpleName();
     }
 }
