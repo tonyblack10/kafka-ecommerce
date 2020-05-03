3fc33685bf8327514d666fa1d7995b5314cd27a9
Tony Costa <tonyblack10@gmail.com>
2020-05-03 10:16:01 -0300

    retries e assincronicidade

--- common-kafka/src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
@@ -1,9 +1,6 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.io.Closeable;
@@ -11,6 +8,7 @@ import java.io.IOException;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 class KafkaDispatcher<T> implements Closeable {
 
@@ -31,6 +29,12 @@ class KafkaDispatcher<T> implements Closeable {
     }
 
     public void send(String topic, String key, CorrelationId id, T payload) throws ExecutionException, InterruptedException {
+        Future<RecordMetadata> future = sendAsync(topic, key, id, payload);
+
+        future.get();
+    }
+
+    public Future<RecordMetadata> sendAsync(String topic, String key, CorrelationId id, T payload) {
         var value = new Message<>(id, payload);
         var record = new ProducerRecord<>(topic, key, value);
 
@@ -42,7 +46,7 @@ class KafkaDispatcher<T> implements Closeable {
             System.out.println("success sending " + data.topic() + ":::partition " + data.partition() + " / offset " + data.offset() + " / timestamp " + data.timestamp());
         };
 
-        producer.send(record, callback).get();
+        return producer.send(record, callback);
     }
 
     @Override
